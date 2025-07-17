95e93ca7b1f48bed391a81814b6edcfcc4b72ad9
Charles Kim <106198850+kharlescim@users.noreply.github.com>
2025-07-17 13:08:32 -0400

    Update LTD_Basic_Model.py

--- LTD_Basic_Model.py
+++ LTD_Basic_Model.py
@@ -6,13 +6,18 @@ os.environ["KERAS_BACKEND"] = "tensorflow"
 import tensorflow as tf
 import pandas as pd
 import keras
-from keras.utils import FeatureSpace
+from keras.optimizers import SGD
+from sklearn.utils import class_weight
+from sklearn.metrics import classification_report
+import collections
 
 import xarray as xr
 import numpy as np
 
 LTD_ds = xr.open_dataset('LTD05.nc')
 spei_ds = xr.open_dataset('spei_obs_3D.nc')
+obs_ds = xr.open_dataset('obs.nc')
+
 LTD = LTD_ds['LTD']
 
 # Creating new time coordinate
@@ -20,6 +25,7 @@ weekly_time = LTD_ds.time.values
 
 # Converting to weekly (method = linear)
 spei_weekly = spei_ds.interp(time=weekly_time, method="linear")
+obs_weekly = obs_ds.interp(time=weekly_time, method="linear")
 
 # Function to convert raw values to percentiles
 # missing values = -999 in obs - might need to alter for proper percentile (7-4)
@@ -30,15 +36,18 @@ def to_percentile(ds, dim='time', missing_val = -999.0):
     return valid.rank(dim=dim, pct=True)
 
 percentiles_spei = to_percentile(spei_weekly)
+percentiles_obs = to_percentile(obs_weekly)
 
 # ens = 1, so safe to ignore it from dataset
 # testing flattening out entire dataset
 spei_df = percentiles_spei.to_dataframe().reset_index()
 LTD_df = LTD.to_dataframe().reset_index()
-merged_df = pd.merge(spei_df, LTD_df, on=['time', 'lat', 'lon'], how='inner')
+obs_df = (percentiles_obs.to_dataframe().reset_index()).drop(columns=['ens'])
+merged_df = pd.merge(spei_df, obs_df, on=['time', 'lat', 'lon'], how='inner')
+merged_df = pd.merge(merged_df, LTD_df, on=['time', 'lat', 'lon'], how='inner')
 
 # cleaning up NaN entries 
-df = merged_df.dropna(subset=['LTD']).copy()
+df = merged_df.dropna().copy()
 
 # changing -1 classification to 5 to work with featurespace
 df["LTD"] = df["LTD"].replace(-1, 5)
@@ -46,91 +55,59 @@ df["LTD"] = df["LTD"].replace(-1, 5)
 # changing datetime to be usable by TensorFlow, changing LTD from float to int to match example
 df["time"] = pd.to_datetime(df["time"]).map(pd.Timestamp.timestamp)
 
+print(collections.Counter(df["LTD"]))
+
 
 val_df = df.sample(frac=0.2, random_state=1337)
 train_df = df.drop(val_df.index)
 
-'''print(
-    "Using %d samples for training and %d for validation"
-    % (len(train_df), len(val_df))
-)'''
-
 def dataframe_to_dataset(dataframe):
     dataframe = dataframe.copy()
-    labels = dataframe.pop("LTD")
-    ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
+    labels = dataframe.pop("LTD").values.astype("int32")
+    features = dataframe.values.astype("float32")
+    ds = tf.data.Dataset.from_tensor_slices((features, labels))
     ds = ds.shuffle(buffer_size=len(dataframe))
     return ds
 
 
-train_ds = dataframe_to_dataset(train_df)
-val_ds = dataframe_to_dataset(val_df)
+train_ds = dataframe_to_dataset(train_df).batch(128).prefetch(tf.data.AUTOTUNE)
+val_ds = dataframe_to_dataset(val_df).batch(128).prefetch(tf.data.AUTOTUNE)
 
-'''for x, y in train_ds.take(1):
-    print("Input:", x)
-    print("Target:", y)'''
+model = keras.Sequential([
+    keras.Input(shape=(35,)),
+    layers.Dense(128, activation="relu"),
+    layers.Dropout(0.3),
+    layers.Dense(64, activation="relu"),
+    layers.Dropout(0.3),
+    layers.Dense(6, activation="softmax"),
+])
 
-train_ds = train_ds.batch(32)
-val_ds = val_ds.batch(32)
-
-feature_space = FeatureSpace(
-    features={
-        "lat": "float",
-        "lon": "float",
-        "time": "float",
-        "SPEI1" : "float",
-        "SPEI3" : "float",
-        "SPEI6" : "float",
-        "SPEI12" : "float",
-        "SPEI24" : "float",
-        "SPEI60" : "float",
-        "SPEI2" : "float",
-        "SPEI9" : "float",
-        "SPEI36" : "float",
-        "SPEI48" : "float",
-        "SPEI72" : "float",
-
-    },
-    output_mode="concat"
+class_weights = class_weight.compute_class_weight(
+    class_weight = 'balanced',
+    classes=np.unique(df["LTD"]),
+    y=df["LTD"]
 )
+class_weight_dict = dict(enumerate(class_weights))
 
-train_ds_with_no_labels = train_ds.map(lambda x, _: x)
-feature_space.adapt(train_ds_with_no_labels)
 
-for x, _ in train_ds.take(1):
-    preprocessed_x = feature_space(x)
-    # print("preprocessed_x.shape:", preprocessed_x.shape)
-    # print("preprocessed_x.dtype:", preprocessed_x.dtype)
 
-preprocessed_train_ds = train_ds.map(
-    lambda x, y: (feature_space(x), y), num_parallel_calls=tf.data.AUTOTUNE
-)
-preprocessed_train_ds = preprocessed_train_ds.prefetch(tf.data.AUTOTUNE)
+ReduceLROnPlateau = tf.keras.callbacks.ReduceLROnPlateau(monitor="val_loss", factor=0.5, patience=2)
+EarlyStopping = tf.keras.callbacks.EarlyStopping(patience=4, restore_best_weights=True)
 
-preprocessed_val_ds = val_ds.map(
-    lambda x, y: (feature_space(x), y), num_parallel_calls=tf.data.AUTOTUNE
-)
-preprocessed_val_ds = preprocessed_val_ds.prefetch(tf.data.AUTOTUNE)
-
-dict_inputs = feature_space.get_inputs()
-encoded_features = feature_space.get_encoded_features()
-
-x = keras.layers.Dense(32, activation="relu")(encoded_features)
-x = keras.layers.Dropout(0.5)(x)
-predictions = keras.layers.Dense(6, activation="softmax")(x) # categories: -1, 0, 1, 2, 3, 4
-
-training_model = keras.Model(inputs=encoded_features, outputs=predictions)
-training_model.compile(
+model.compile(
     optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
 )
-
-inference_model = keras.Model(inputs=dict_inputs, outputs=predictions)
-
-# val_accuracy is plateauing wayyy too early at .2678 
-# looks like model can't learn useful patterns from time, lat and lon alone - which is kind of expected 
-training_model.fit(
-    preprocessed_train_ds,
+ 
+model.fit(
+    train_ds,
     epochs=20,
-    validation_data=preprocessed_val_ds,
+    validation_data=val_ds,
     verbose=2,
+    # class_weight=class_weight_dict
+    callbacks=[ReduceLROnPlateau, EarlyStopping]
 )
+
+y_true = np.concatenate([y.numpy() for _, y in val_ds], axis=0)
+y_pred = np.argmax(model.predict(val_ds), axis=1)
+
+print(classification_report(y_true, y_pred, digits=3))
